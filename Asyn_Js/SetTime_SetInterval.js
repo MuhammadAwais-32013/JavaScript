a8ae6eeaaf84c813fc7c639078bc958454c0f14c
Muhammad Awais Qarni <owaisq2019@gmail.com>
2024-08-16 13:18:10 +0500

    update SetT_I

--- Asyn_Js/SetTime_SetInterval.js
+++ Asyn_Js/SetTime_SetInterval.js
@@ -9,7 +9,44 @@ let timerId= setTimeout(function SayHi(){
     
 },2000)
 
-// let timerId = setTimeout(function tick() {
-//     console.log('tick');
-//     timerId = setTimeout(tick, 2000); // (*)
-//   }, 2000);
+let timerIds = setTimeout(function tick() {
+    console.log('tick');
+    timerIds = setTimeout(tick, 2000); // (*)
+  }, 2000);
+
+
+// *************************Get Sum of Array*********************
+
+let data=function uer(sum){
+    setInterval(()=>{
+        sum([1,2,3,5,6])
+    },2000)
+}
+
+function sum(number) {
+    let sum=0;
+    for (const element of number) {
+        sum+=element;
+    }
+    console.log(`Sum is : ${sum}`);
+    
+}
+
+// data(sum);
+
+let sumAsyn=(callback)=>{
+    function repeat() {
+        callback([1,2,3,4,5]);
+        setTimeout(repeat,2000)
+    }
+    repeat();
+}
+
+sumAsyn(sum)
+// let arr=[2,3,4,5]
+
+// let sum=0;
+//     for (const element of number) {
+//         sum+=element;
+//     }
+//     console.log(`Sum is : ${sum}`);
