596fb2d4082c65f197209a1a807ec50c97f35071
Muhammad Awais Qarni <owaisq2019@gmail.com>
2024-08-16 12:14:40 +0500

    added

--- Asyn_Js/SetTime_SetInterval.js
+++ Asyn_Js/SetTime_SetInterval.js
@@ -1,8 +1,13 @@
-let timerId= setTimeout(()=>{
+let timerId= setTimeout(function SayHi(){
     console.log("Parent");
-    setTimeout(()=>{
+    timerId=setTimeout(()=>{
         console.log("Child");
         
     },2000)
     
 },2000)
+
+// let timerId = setTimeout(function tick() {
+//     console.log('tick');
+//     timerId = setTimeout(tick, 2000); // (*)
+//   }, 2000);
